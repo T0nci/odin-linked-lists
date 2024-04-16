5d451b219218085177e9f58ff1ca412eda6320e9
T0nci <138928016+T0nci@users.noreply.github.com>
2024-04-16 18:25:18 +0200

    Add a function to append values
    Also changed the head's name and made it a local variable accessed
    through closures so the user can't manipulate our object directly.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 class LinkedList {
   constructor() {
-    this.nextNode = null;
+    let head = null;
 
     class Node {
       constructor(value=null) {
@@ -10,12 +10,12 @@ class LinkedList {
     }
 
     this.toString = function() {
-      if (this.nextNode === null) {
-        return 'head -> null';
+      if (head === null) {
+        return 'null';
       }
 
-      let node = this.nextNode;
-      let string = 'head -> null';
+      let node = head;
+      let string = '';
       while (node !== null) {
         string += `( ${node.value} ) -> `;
         node = node.nextNode;
@@ -23,9 +23,25 @@ class LinkedList {
 
       return string + 'null';
     };
+
+    this.append = function(value) {
+      if (head === null) {
+        head = new Node(value);
+        return;
+      }
+
+      let node = head;
+      while(node.nextNode !== null) {
+        node = node.nextNode;
+      }
+
+      node.nextNode = new Node(value);
+    }
   }
 }
 
 const list = new LinkedList();
-
+list.append(3);
+list.append(2);
+list.append(1);
 console.log(list.toString());
